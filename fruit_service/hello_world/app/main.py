f030f6db18b382f4b554e4a34134d3a486b5c7cb
Adel NOUAR <adel.nouar@protonmail.com>
2022-06-11 17:11:13 +0200

    test logging with hello_world

--- fruit_service/hello_world/app/main.py
+++ fruit_service/hello_world/app/main.py
@@ -1,7 +1,12 @@
+import logging
 from http import HTTPStatus
 from fastapi import FastAPI
 
 from app.routers import hello
+
+logging.basicConfig(encoding='utf-8', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+logger = logging.getLogger(__name__)
 
 app = FastAPI(
     title='Hellooooo Wooorld',
--- fruit_service/hello_world/server.py
+++ fruit_service/hello_world/server.py
@@ -1,5 +1,4 @@
-from imp import reload
 import uvicorn
 
 if __name__ == '__main__':
-    uvicorn.run('app.main:app', reload=True)
+    uvicorn.run('app.main:app', reload=True, log_level='warning')
